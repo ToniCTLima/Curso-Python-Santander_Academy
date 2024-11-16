7f0390af3f89612becba7f845fcbb7a4b31ec1e8
Toni <tonictlima@gmail.com>
2024-11-16 10:20:58 -0300

    1 - Conceitos básico de sintaxe
    Conceitos básicos de sintaxe em Python.

---
+++ 1-Conceitos Básicos sintaxe.py
@@ -0,0 +1,48 @@
+""" 
+Indentação - No Python, a identação (espaços ou tabulações no início de uma linha) é utilizado para delimitar blocos de código. Diferente de outras linguagens que utilizam chaves
+ ou palavaras-chave, o Python utiliza identação para determinar o escopo das declarações. Por exemplo:
+
+ if condition:
+    # Bloco de código se a condição for verdadeira
+
+    instrucao1
+    instrucao2
+
+else:
+    # Bloco de código se a condição for falsa
+
+    instrucao3
+    instrucao4
+
+COMENTÁRIOS
+
+Os comentários são linas de texto no código que são ignoradas pelo interpretador do Python. Eles são utilizado para explicar ou documentar o código. No Python, os comentários
+de uma única linha começam com o símbolo #, enquanto os comentários de várias linhas são delimitados por três aspas. Por exemplo:
+
+# Este é um comentário de uma única linha.
+"""
+"""
+Este é um comentário
+de várias linhas.
+"""
+
+""""
+SENSIBILIDADE A MAIÚSCULA E minúscula.
+
+Python distingue entre maiúscula e minúsculas. Portanto, variável, Variável, VARIÁVEL são consideradas variáveis deiferentes.
+
+PONTO E VÍRGULA.
+
+Diferente de outras linguagens, o Python não requer o uso de ponto e vírgula (;) ao final de cada instrução. No entanto, se você deseja escrever várias indtruções em uma única linha, pode separá-las com um ponto e vírgula. Por exemplo:
+
+instruca1; instrucao2; instrucao3
+
+USO DE PARÊNTESES
+
+Os parênteses são utilizadas para agrupar expressões, definir funções e realizar chamadas e funções. Por exemplo:
+
+resultado = (a + b) * c
+
+
+
+"""
